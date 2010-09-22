b805c3a2a13cbb5a268dd3343d17551970c30c22
jacobkwitkoski@gmail.com <jacobkwitkoski@gmail.com@5669f243-eaf7-85b2-e596-d29227da744b>
2010-09-22 01:24:46 +0000

    Base structure for project in Visual Studio 2010.
    See SVN_Policy.docx regarding which files should be included.

---
+++ Angels_Vs_Demons/Angels_Vs_Demons/Angels_Vs_Demons/Screens/HvHMenuScreen.cs
@@ -0,0 +1,16 @@
+﻿#region Using Statements
+using System;
+using System.Threading;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+#endregion
+
+namespace Angels_Vs_Demons
+{
+    class HvHMenuScreen : GameplayScreen
+    {
+
+    }
+}
