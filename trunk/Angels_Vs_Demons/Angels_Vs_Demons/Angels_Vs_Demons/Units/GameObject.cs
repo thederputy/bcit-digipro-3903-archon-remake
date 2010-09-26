c6ca41593890e28f08fce4c07266189fa101887a
beamer1989 <beamer1989@5669f243-eaf7-85b2-e596-d29227da744b>
2010-09-26 07:59:34 +0000

    Added the game object class which contains a texture, position and direction. Created a 2d grid in the gamescreen class with modifiable dimensions and tile size though is not formatted to the right area of game screen and currently contains a placeholder texture for the tiles

---
+++ trunk/Angels_Vs_Demons/Angels_Vs_Demons/Angels_Vs_Demons/Units/GameObject.cs
@@ -0,0 +1,25 @@
+﻿#region Using Statements
+using System;
+using System.Threading;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+#endregion
+
+namespace Angels_Vs_Demons
+{
+    class GameObject
+    {
+        public Texture2D sprite;
+        public Vector2 position;
+        public float direction;
+        public Rectangle rect;
+        public GameObject(Texture2D loadedTexture)
+        {
+            direction = 0.0f;
+            position = Vector2.Zero;
+            sprite = loadedTexture;
+        }
+    }
+}
