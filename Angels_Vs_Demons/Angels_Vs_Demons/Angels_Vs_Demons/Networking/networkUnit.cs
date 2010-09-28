668581a69a349b85d4e38ed54d8a53d54f1fc924
jacobkwitkoski@gmail.com <jacobkwitkoski@gmail.com@5669f243-eaf7-85b2-e596-d29227da744b>
2010-09-28 21:11:36 +0000

    - Implemented networkedGameMenuScreen.cs using networkUnit.cs - modified the Content manager in GameplayScreen to be public, as the children need access to it.

---
+++ Angels_Vs_Demons/Angels_Vs_Demons/Angels_Vs_Demons/Networking/networkUnit.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace Angels_Vs_Demons.Networking
+{
+    class networkUnit : AllUnits
+    {
+        int screenWidth = 853;
+        int screenHeight = 480;
+        public Vector2 Velocity;
+
+        Vector2 screenSize;
+        public Vector2 unitInput;
+
+        public networkUnit(Texture2D loadedTexture, int gamerIndex, ContentManager manager)
+            : base(loadedTexture)
+        {
+            position.X = screenWidth / 4 + (gamerIndex % 5) * screenWidth / 8;
+            position.Y = screenHeight / 4 + (gamerIndex / 5) * screenHeight / 5;
+
+            screenSize = new Vector2(screenWidth, screenHeight);
+        }
+
+        /// <summary>
+        /// Draws the unit.
+        /// </summary>
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            Vector2 origin = new Vector2(sprite.Width / 2, sprite.Height / 2);
+
+            spriteBatch.Draw(sprite, position, null, Color.White,
+                             0, origin, 1, SpriteEffects.None, 0);
+        }
+
+        /// <summary>
+        /// Moves the unit in response to the current input settings.
+        /// </summary>
+        public void Update()
+        {
+            // If we have finished turning, also start moving forward.
+            Velocity += unitInput;
+
+            // Update the position and velocity.
+            position += Velocity;
+
+            // Clamp so the unit cannot drive off the edge of the screen.
+            position = Vector2.Clamp(position, Vector2.Zero, screenSize);
+        }
+    }
+}
