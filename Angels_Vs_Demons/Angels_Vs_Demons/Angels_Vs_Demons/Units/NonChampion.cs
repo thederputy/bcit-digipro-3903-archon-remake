96065fca14f2f87992a8ac0bf0ac6907c02d2c79
enterintoexit <enterintoexit@5669f243-eaf7-85b2-e596-d29227da744b>
2010-09-28 23:44:35 +0000

    -Actually added the Unit and NonChampion cs files this time...

---
+++ Angels_Vs_Demons/Angels_Vs_Demons/Angels_Vs_Demons/Units/NonChampion.cs
@@ -0,0 +1,85 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
+
+namespace Angels_Vs_Demons
+{
+    #region Enumeration Classes
+
+
+    /// <summary>
+    /// Types of attacks. All non-champion units have an attack type. Attack type interacts with Unit.armorType.
+    /// </summary>
+    public enum attackType
+    {
+        MELEE,
+        PROJECTILE,
+        MAGIC
+    }
+
+
+    #endregion
+
+
+    /// <summary>
+    /// NonChampion represents all non-champion units.
+    /// </summary>
+    abstract class NonChampion : Unit
+    {
+        #region Properties
+
+
+        /// <summary>
+        /// Atack Power represents the amount of damage this unit inflicts before mitigation.
+        /// </summary>
+        public int AttackPower
+        {
+            get { return attackPower; }
+            set { attackPower = value; }
+        }
+
+        private int attackPower;
+
+        /// <summary>
+        /// The attack type property of this unit (MELEE, PROJECTILE, ETC).
+        /// </summary>
+        public attackType AttackTypeVal
+        {
+            get { return attackTypeVal; }
+            set { attackTypeVal = value; }
+        }
+
+        private attackType attackTypeVal;
+        
+
+        public int Range
+        {
+            get { return range; }
+            set { range = value; }
+        }
+
+        private int range;
+
+
+        #endregion
+
+        protected NonChampion(Texture2D loadedTexture)
+            : base(loadedTexture)
+        {
+
+        }
+
+        #region Public Methods
+
+
+        public abstract void attack(Unit victim);
+
+
+        #endregion
+
+    }
+}
