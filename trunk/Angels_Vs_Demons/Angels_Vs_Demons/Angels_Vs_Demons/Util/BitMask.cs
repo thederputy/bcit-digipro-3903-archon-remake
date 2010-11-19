52e8a3f2be699392801364e3c5f36d6912ba13ab
jacobkwitkoski@gmail.com <jacobkwitkoski@gmail.com@5669f243-eaf7-85b2-e596-d29227da744b>
2010-11-19 17:13:52 +0000

    - Included BitMask file this time.

---
+++ trunk/Angels_Vs_Demons/Angels_Vs_Demons/Angels_Vs_Demons/Util/BitMask.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Angels_Vs_Demons.Util
+{
+    static class BitMask
+    {
+        public static int[] bits = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288 };
+
+        public static int bitsTotal = initializeBitsTotal();
+
+        public static int[] angelBits = { 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288 };
+
+        public static int[] demonBits = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512 };
+
+        public static int angelTotal = initializeAngelTotal();
+
+        public static int demonTotal = initializeDemonTotal();
+
+        private static int initializeBitsTotal()
+        {
+            int total = 0;
+            for (int i = 0; i < bits.Length; i++)
+            {
+                total += bits[i];
+            }
+            return total;
+        }
+
+        private static int initializeAngelTotal() {
+            int total = 0;
+            for(int i = 0; i < angelBits.Length; i++)
+            {
+                total += angelBits[i];
+            }
+            return total;
+        }
+
+        private static int initializeDemonTotal()
+        {
+            int total = 0;
+            for (int i = 0; i < demonBits.Length; i++)
+            {
+                total += demonBits[i];
+            }
+            return total;
+        }
+    }
+}
