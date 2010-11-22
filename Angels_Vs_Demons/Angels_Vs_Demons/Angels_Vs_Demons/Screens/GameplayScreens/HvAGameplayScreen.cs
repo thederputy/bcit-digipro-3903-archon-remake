920b8dc2fc6b67470f5a76a88e21244350c3ac65
jacobkwitkoski@gmail.com <jacobkwitkoski@gmail.com@5669f243-eaf7-85b2-e596-d29227da744b>
2010-11-22 17:15:02 +0000

    - Changed tons of stuff. Got attacking sort of working, need to debug some stuff with attacking though.

--- Angels_Vs_Demons/Angels_Vs_Demons/Angels_Vs_Demons/Screens/GameplayScreens/HvAGameplayScreen.cs
+++ Angels_Vs_Demons/Angels_Vs_Demons/Angels_Vs_Demons/Screens/GameplayScreens/HvAGameplayScreen.cs
@@ -5,21 +5,53 @@ using System.Linq;
 using System.Text;
 using Angels_Vs_Demons.BoardObjects;
 using Angels_Vs_Demons.GameObjects;
+using Angels_Vs_Demons.Players;
 #endregion
 
 namespace Angels_Vs_Demons.Screens.GameplayScreens
 {
+    /// <summary>
+    /// Represents a Human vs AI game.
+    /// </summary>
     class HvAGameplayScreen : GameplayScreen
     {
+        #region Initialization
 
+        /// <summary>
+        /// Constructs a gameplay screen with one human player and one AI player.
+        /// </summary>
+        public HvAGameplayScreen()
+            :base()
+        {
+            player1 = new HumanPlayer(Faction.ANGEL);
+            player2 = new ComputerPlayer(Faction.DEMON);
+        }
+
+        #endregion
+
+        #region Move/Attack Phases
+
+        /// <summary>
+        /// Executes the move phase for a human vs AI game.
+        /// </summary>
+        /// <param name="currentTile">the tile that the cursor is now on.</param>
+        /// <param name="boardSelectedTile">the tile that was selected</param>
         protected override void executeMovePhase(Tile currentTile, Tile boardSelectedTile)
         {
-
+            board.applyMove(new Move(currentTile, boardSelectedTile));
         }
 
-        protected override void executeAttackPhase(Tile currentTile, Tile boardSelectedTile)
+
+        /// <summary>
+        /// Executes the attack phase for a Human vs AI game.
+        /// </summary>
+        /// <param name="victimTile">the tile that is getting attacked</param>
+        /// <param name="attackerTile">the tile that is attacking</param>
+        protected override void executeAttackPhase(Tile victimTile, Tile attackerTile)
         {
-
+            board.applyAttack(new Attack(victimTile, attackerTile));
         }
+
+        #endregion
     }
 }
--- Angels_Vs_Demons/Angels_Vs_Demons/Angels_Vs_Demons/Screens/GameplayScreens/HvHGameplayScreen.cs
+++ Angels_Vs_Demons/Angels_Vs_Demons/Angels_Vs_Demons/Screens/GameplayScreens/HvHGameplayScreen.cs
@@ -7,34 +7,53 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Angels_Vs_Demons.BoardObjects;
 using Angels_Vs_Demons.GameObjects;
+using Angels_Vs_Demons.Players;
 #endregion
 
 namespace Angels_Vs_Demons.Screens.GameplayScreens
 {
+    /// <summary>
+    /// Represents a Human vs Human (hotseat) game.
+    /// </summary>
     class HvHGameplayScreen : GameplayScreen
     {
+        #region Initialization
+
         /// <summary>
-        /// Executes the move phase for a hotseat game
+        /// Constructs a gameplay screen with two human players.
         /// </summary>
+        public HvHGameplayScreen()
+            :base()
+        {
+            player1 = new HumanPlayer(Faction.ANGEL);
+            player2 = new HumanPlayer(Faction.DEMON);
+        }
+
+        #endregion
+
+        #region Move/Attack Phases
+
+        /// <summary>
+        /// Executes the move phase for a hotseat game.
+        /// </summary>
+        /// <param name="currentTile">the tile that the cursor is now on.</param>
+        /// <param name="boardSelectedTile">the tile that was selected</param>
         protected override void executeMovePhase(Tile currentTile, Tile boardSelectedTile)
         {
             board.applyMove(new Move(currentTile, boardSelectedTile));
         }
 
+
         /// <summary>
-        /// Processes the attack phase for a hotseat game.
+        /// Executes the attack phase for a hotseat game.
         /// </summary>
-        protected override void processAttackPhase()
+        /// <param name="victimTile">the tile that is getting attacked</param>
+        /// <param name="attackerTile">the tile that is attacking</param>
+        protected override void executeAttackPhase(Tile victimTile, Tile attackerTile)
         {
-            ///this attack is just default for now, until we have attacking working
-            Attack attack = new Attack(null, null);
-            board.applyAttack(attack);
-            board.endTurn();
+            board.applyAttack(new Attack(victimTile, attackerTile));
         }
 
-        protected override void executeAttackPhase(Tile currentTile, Tile boardSelectedTile)
-        {
-
-        }
+        #endregion
     }
 }
