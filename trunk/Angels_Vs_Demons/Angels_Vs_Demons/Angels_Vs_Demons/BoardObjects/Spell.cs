d79e50b569370025d657cc914a237b7153beb69e
beamer1989 <beamer1989@5669f243-eaf7-85b2-e596-d29227da744b>
2010-11-24 00:36:46 +0000

    Added Spell.cs, no functionality

---
+++ trunk/Angels_Vs_Demons/Angels_Vs_Demons/Angels_Vs_Demons/BoardObjects/Spell.cs
@@ -0,0 +1,13 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Angels_Vs_Demons.BoardObjects
+{
+    class Spell : Attack
+    {
+        
+    }
+}
+*/
